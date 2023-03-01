6293bd9bef7c651fc8f9e8d4d3bba1c1d0d049fe
paulinegallezot <paulinegallezot@gmail.com>
2023-03-01 21:29:35 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+function toggleChildren() {
+  let children = document.getElementsByClassName('child')
+  for (let i = 0; i < children.length; i++) {
+    if (children[i].style.display === 'block') {
+      children[i].style.display = 'none'
+      let grandchildren = children[i].getElementsByClassName('grandchild')
+      for (let j = 0; j < grandchildren.length; j++) {
+        grandchildren[j].style.display = 'none'
+      }
+    } else {
+      children[i].style.display = 'block'
+    }
+  }
+}
+
+function toggleGrandchildren(child) {
+  let grandchildren = child.getElementsByClassName('grandchild')
+  for (let i = 0; i < grandchildren.length; i++) {
+    if (grandchildren[i].style.display === 'block') {
+      grandchildren[i].style.display = 'none'
+    } else {
+      grandchildren[i].style.display = 'block'
+    }
+  }
+}
